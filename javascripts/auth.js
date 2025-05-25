70e2bf76cfedb1dc270288a69ba6f9766460c7f4
akhilub <60491139+akhilub@users.noreply.github.com>
2025-05-25 10:46:43 +0000

    Deploying to gh-pages from @ akhilub/DSA-Bible-Source@c27ff942bd8aee88d943d10ace47834ff84bfe43 🚀

--- javascripts/auth.js
+++ javascripts/auth.js
@@ -60,15 +60,11 @@ const renderContent = (isAuthenticated) => {
           })
         }
 
-        // Reinitialize Material for MkDocs components
-        if (
-          typeof document$ !== "undefined" &&
-          (typeof document$.next === "function" ||
-            typeof document$.subscribe === "function")
-        ) {
-          // This will trigger the document$ observable which should reinitialize components
-          document$.next(document)
-        }
+        // // Reinitialize Material for MkDocs components
+        // if (typeof document$.subscribe === "function") {
+        //   // This will trigger the document$ observable which should reinitialize components
+        //   document$.next(document)
+        // }
       }
     } else {
       // User is not authenticated - show login prompt
